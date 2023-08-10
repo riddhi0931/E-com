864242cfe8f5c562036b035a771c80e4c627bef2
BhaveshW09 <bhaveshwr09@gmail.com>
2023-08-10 10:01:31 +0530

    h1pushed

--- src/Pages/AllProducts.js
+++ src/Pages/AllProducts.js
@@ -1,29 +1,25 @@
-import React, { useContext } from 'react'
-import { GlobalData } from '../AGlobalContext/GlobalContext';
-import Products from '../Components/Products';
+import React, { useContext } from "react";
+import { GlobalData } from "../AGlobalContext/GlobalContext";
+import Products from "../Components/Products";
 
 const AllProducts = () => {
+  const { allproducts } = useContext(GlobalData);
 
-    const { allproducts } = useContext(GlobalData)
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Lu</h2>
 
-    return (
-        <>
-            <h1>Hello</h1>
+      <div className="w-[1280px] m-auto">
+        <h1 className="text-3xl font-bold mt-20 my-10">All Products</h1>
+        <div className=" flex flex-rows gap-4 border border-black w-[1000px]">
+          {allproducts.map((item) => (
+            <Products key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-            <div className='w-[1280px] m-auto'>
-                <h1 className='text-3xl font-bold mt-20 my-10'>All Products</h1>
-                <div className=' flex flex-rows gap-4 border border-black w-[1000px]'>
-                    {
-                        allproducts.map((item) => (
-                            <Products key={item.id} item={item} />
-                        ))
-                    }
-                </div>
-            </div>
-
-        </>
-
-    )
-}
-
-export default AllProducts
+export default AllProducts;
